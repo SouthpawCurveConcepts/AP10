3a8825789aef49c7e85e07b66af5afb0c3d50ffe
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-01-15 12:04:58 -0600

    AP10 first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>AP10</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#B0DFB6',
     alignItems: 'center',
     justifyContent: 'center',
   },
